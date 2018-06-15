1fb6c80129cb9c1340dc51ad9e4376c3d7df6310
solisgb <37953927+solisgb@users.noreply.github.com>
2018-06-15 09:27:28 +0200

    First version working

--- XYHL.py
+++ XYHL.py
@@ -43,53 +43,53 @@ def make_graphs(project):
 
     select_data = project.find('select_data').text.strip()
     npar = select_data.count('?')
-    if npar > 0:
-        par_select_data_col = project.findall('select_master/parameter_select_data_column')
-        if len(par_select_data_col) != npar:
-            raise ValueError('distinto número de ? en select_data que tags par_select_data_col')
-        cols = [int(item.text)-1 for item in par_select_data_col]
+    if npar != 1:
+        raise ValueError('select_data debe tener un signo ?')
+    id_col = int(project.find('select_master').get('id_column')) - 1
 
     select_umbrales = project.find('select_umbrales').text.strip()
     if select_umbrales.count('?') != 1:
-        raise ValueError('select_umbrales debe tener un solo ?')
+        raise ValueError('select_umbrales debe tener un signo ?')
+    umbral_col = int(project.find('select_umbrales').get('umbral_column')) - 1
 
     cur2 = con.cursor()
-    fecha_col = int(project.find('select_data').get('fecha_column'))-1
-    value_col = int(project.find('select_data').get('value_column'))-1
+    fecha_col = int(project.find('select_data').get('fecha_column')) - 1
+    value_col = int(project.find('select_data').get('value_column')) - 1
     dir_out = project.find('dir_out').text.strip()
     ylabel = project.find('graph').get('y_axis_name')
     for row in cur:
 
-        params = tuple([row[col] for col in cols])
-        cur2.execute(select_data, params)
+        # datos de la serie
+        cur2.execute(select_data, row[id_col])
         xy = [(row_data[fecha_col], row_data[value_col]) for row_data in cur2]
-        nd = len(xy)
-        if nd > 1:
-            print('{0} tiene {1:d} datos entre {2} y {3}'.format(row[cols[0]],
-                  nd, xy[0][0].strftime('%d/%m/%Y'),
-                  xy[-1][0].strftime('%d/%m/%Y')))
-        else:
-            print('{0} tiene {1:d} datos'.format(row[cols[0]], nd))
+        if len(xy) == 0:
+            print('{0} no tiene datos'.format(row[id_col]))
             continue
         fechas = [xy1[0] for xy1 in xy]
         values = [xy1[1] for xy1 in xy]
 
-        cur2.execute(select_umbrales, params[0])
-        umbrales = [row_u for row_u in cur2]
-        nd = len(row_u)
-        if nd == 0:
-            print('{} no tiene definición de umbrales'.format(params[0]))
+        # datos de los umbrales
+        cur2.execute(select_umbrales, row[id_col])
+        rows_u = [row_u for row_u in cur2]
+        values_u = [[row1_u[umbral_col],
+                     row1_u[umbral_col]] for row1_u in rows_u]
+        if len(values_u) == 0:
+            print('{0} no tiene umbrales'.format(row[id_col]))
             continue
 
-        fechas_u = fechas_u_get(row_u, fechas)
-        values_u = values_u_get(row_u)
+        # todos los umbrales se ponen en el rango de datos de cada sondeo
+        # si se desea ponerlo en su rango específico debe llamarse a la
+        # función fechas_u_get(row_u, fechas)
+        fechas_u = [fechas[0], fechas[-1]]
 
         stitle = get_title(project, row)
-        legends = legends_get(project, row)
+        legend_master = legend_master_get(project, row)
+        legends_umbrales = legends_umbrales_get(project, rows_u)
         file_name = file_name_get(project, row)
         dst = join(dir_out, file_name)
 
-#        XYt_1(fechas, values, fechas_u, values_u, stitle, ylabel, legends, dst)
+        XYt_1(fechas, values, legend_master, fechas_u, values_u,
+              legends_umbrales, stitle, ylabel, dst)
 
     con.close()
 
@@ -111,19 +111,33 @@ def get_title(project, row):
     return '\n'.join(stitles)
 
 
-def legends_get(project, row):
+def legend_master_get(project, row):
     """
-    forma las leyendas del gráfico
+    forma la leyenda de la serie principal del gráfico
     """
-    legends = project.findall('graph/legend')
-    if len(legends) != 2:
-        raise ValueError('El número de tags legend debe ser 2')
-    slegends, columns = zip(*[[legend.get('text'),
-                      int(legend.get('column'))-1] for legend in legends])
-    slegends = list(slegends)
-    for i, column in enumerate(columns):
-        slegends[i] = slegends[i].format(row[column])
-    return slegends
+    legend_master = project.find('graph/legend_master').text.strip()
+    columns_master = project.findall('graph/legend_master/column')
+    if len(columns_master) == 0:
+        return legend_master
+    subs = [row[int(col1.text)-1] for col1 in columns_master]
+    return legend_master.format(*subs)
+
+
+def legends_umbrales_get(project, rows):
+    """
+    forma la leyenda de cada uno de los umbrales
+    """
+    titles = project.findall('graph/legend_umbrales')
+    if len(titles) == 0:
+        return ""
+    stitles = [title.text.strip() for title in titles]
+    for i, (title, row) in enumerate(zip(titles, rows)):
+        cols = title.findall('column')
+        if len(cols) == 0:
+            continue
+        subs = [row[int(col.text)-1] for col in cols]
+        stitles[i] = stitles[i].format(*subs)
+    return stitles
 
 
 def file_name_get(project, row):
@@ -137,65 +151,64 @@ def file_name_get(project, row):
     return sname
 
 
-def fechas_u_get(row_u, fechas):
-    """
-    forma la lista de fechas de los umbrales
-    """
-    fechas_u = []
-    for row_u1 in row_u:
-        if fechas[0] > row_u1[2]:
-            fecha_u1 = fechas[0]
-        else:
-            fecha_u1 = row_u1[2]
-        if row_u1[3] is None:
-            fecha_u2 = fechas[-1]
-        elif len(row_u1[3].strip()) == 0:
-            fecha_u2 = fechas[-1]
-        elif fechas[0] < row_u1[3]
-            fecha_u2 = fechas[-1]
-        else:
-            fecha_u2 = row_u1[3]
-        fechas_u.append((fecha_u1, fecha_u2))
-    return fechas_u
-
-
-def values_u_get(row_u):
-    """
-    forma la lista de valores de los umbrales
-    """
-    values_u = [(row_u1[1], row_u1[1]) for row_u1 in row_u:]
-    return values_u
-
-
-def XYt_1(xdate, y, stitle, ylabel, dst):
+def XYt_1(fechas, values, legend_master, fechas_u, values_u, legends_umbrales,
+          stitle, ylabel, dst):
     """
     XY x datetime serie, y serie
     """
-
-#    import datetime
-#    import numpy as np
+    import matplotlib.pyplot as mpl
     import matplotlib.pyplot as plt
     import matplotlib.dates as mdates
 
-    dateFmt = mdates.DateFormatter('%m/%Y')
+    dateFmt = mdates.DateFormatter('%d-%m-%Y')
 
     fig, ax = plt.subplots()
-    ax.plot(xdate, y)
+    ax.plot(fechas, values, marker='.', label=legend_master)
+    for i, vu1 in enumerate(values_u):
+        ax.plot(fechas_u, vu1, label=legends_umbrales[i])
 
-    plt.grid(True)
     plt.ylabel(ylabel)
-
     # rotate and align the tick labels so they look better
     fig.autofmt_xdate()
 
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
 
-    # use a more precise date string for the x axis locations in the
-    # toolbar
     ax.xaxis.set_major_formatter(dateFmt)
     ax.set_title(stitle)
+    mpl.legend(loc='best', framealpha=0.5)
+    mpl.legend(loc='best', framealpha=0.5)
+    mpl.tight_layout()
+    mpl.grid(True)
 
-    plt.tight_layout()
     fig.savefig(dst)
     plt.close('all')
+
+
+# def fechas_u_get(row_u, fechas):
+#    """
+#    forma la lista de fechas de los umbrales
+#    TODO: si se desea implementar hay que terminar de escribirla
+#    ahora no funciona
+#    """
+#    fechas_u = []
+#    for row_u1 in row_u:
+#        fecha_u1 = None
+#        for fecha1 in fechas:
+#            if fecha1 >= row_u1[2]:
+#                fecha_u1 = fecha1
+#                break
+#
+#        if row_u1[3] is None:
+#            fecha_u2 = fechas[-1]
+#            break
+#
+#        elif len(row_u1[3].strip()) == 0:
+#            fecha_u2 = fechas[-1]
+#        elif fechas[0] < row_u1[3]:
+#            fecha_u2 = fechas[-1]
+#        else:
+#            fecha_u2 = row_u1[3]
+#        fechas_u.append((fecha_u1, fecha_u2))
+#    return fechas_u
+#
